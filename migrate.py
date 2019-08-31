3deb193232ea02bb9f758ccb568d451c20a0c718
Danilo Bargen <mail@dbrgn.ch>
2019-09-01 00:08:53 +0200

    Remove listdb.py, add migrate.py

--- migrate.py
+++ migrate.py
@@ -1,14 +1,15 @@
 """
-List all URLs in the shelve database.
+Migrate a Python2 shelve database to a JSON based database.
 
 Usage:
-    listdb.py <database>
+    listdb.py <old.db> <new.json>
     listdb.py --help
     listdb.py --version
 
 """
 from __future__ import print_function, division, absolute_import, unicode_literals
 
+import json
 import sys
 import signal
 import shelve
@@ -18,16 +19,20 @@ from docopt import docopt
 
 def sigint(signal, frame):
     """Handle SIGINT signal to properly close shelve."""
-    global db
-    db.close()
+    global db_old
+    db_old.close()
     sys.exit()
 signal.signal(signal.SIGINT, sigint)
 
 
 if __name__ == '__main__':
     args = docopt(__doc__, version='Churz v0.1')
-    global db
-    db = shelve.open(str(args['<database>']))
-    for pair in db.iteritems():
+    global db_old
+    db_old = shelve.open(str(args['<old.db>']))
+    data = {}
+    for pair in db_old.items():
         print('%s -> %s' % pair)
-    db.close()
+        data[pair[0]] = pair[1]
+    with open(str(args['<new.json>']), 'w') as db_new:
+        db_new.write(json.dumps(data, indent=2))
+    db_old.close()
